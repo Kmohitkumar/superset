813e44ed2db357d20c93a57f65ae4c6e9c908340
Kmohitkumar <mohitkumar.k@celllabs.com>
2025-03-24 12:14:03 +0530

    feat: display only loading indicator while fetching data from database

--- superset-frontend/src/components/Chart/Chart.tsx
+++ superset-frontend/src/components/Chart/Chart.tsx
@@ -276,9 +276,10 @@ class Chart extends PureComponent<ChartProps, {}> {
   }
 
   renderSpinner(databaseName: string | undefined) {
-    const message = databaseName
-      ? t('Waiting on %s', databaseName)
-      : t('Waiting on database...');
+    const message = t(' ');
+    // databaseName
+    //   ? t('Waiting on %s', databaseName)
+    //   : t('Waiting on database...');
 
     return (
       <LoadingDiv>
